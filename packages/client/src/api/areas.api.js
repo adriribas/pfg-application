b7bf64e0a336fd248277d2157357de2d58217eb1
Adrià Ribas <ribasadria00@gmail.com>
2022-10-20 10:25:13 +0200

    [Front-end]: Implemented Responsables management view

--- packages/client/src/api/areas.api.js
+++ packages/client/src/api/areas.api.js
@@ -5,3 +5,5 @@ export const get = abv => axios.get(`/areas/${abv}`);
 
 export const list = ({ params, filterData, associations } = {}) =>
   axios.post('/areas/filter', formatFilter(filterData, associations), params);
+
+export const update = (abv, data) => axios.patch(`/areas/${abv}`, data);
--- packages/client/src/views/directors/GestResponsablesView.vue
+++ packages/client/src/views/directors/GestResponsablesView.vue
@@ -1,3 +1,36 @@
-<script></script>
-<template><h1>Gestió de Responsables de Docència</h1></template>
-<style></style>
+<script setup>
+import { areasApi } from '@/api';
+import AssignationTable from '@/components/usersManagement/AssignationTable.vue';
+
+const columns = [
+  { name: 'abv', label: 'Abreviació', field: 'abv', align: 'center' },
+  { name: 'name', label: 'Àrea', field: 'name', align: 'left' }
+];
+
+const loadData = async () => {
+  const { data: areas } = await areasApi.list({
+    params: { fields: 'abv,name,responsable' }
+  });
+
+  return areas;
+};
+
+const updateUserAssignation = (areaAbv, userId = null) => areasApi.update(areaAbv, { responsable: userId });
+</script>
+
+<template>
+  <q-page padding>
+    <AssignationTable
+      row-key="abv"
+      role="responsable de docència"
+      pluralized-role="responsables de docència"
+      data-entity-label="de les àrees"
+      :data-entity-columns="columns"
+      user-label="Responsable"
+      user-field="responsable"
+      :data-loader="loadData"
+      :user-assignation-updater="updateUserAssignation" />
+  </q-page>
+</template>
+
+<style lang="sass" scoped></style>
